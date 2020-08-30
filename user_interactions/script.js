9aabe1e44152459958107124b5fa85b38280add2
Vladislav Dankevich <vielendanke1991@gmail.com>
2020-08-30 22:26:38 +0600

    user interactions by listeners

--- user_interactions/script.js
+++ user_interactions/script.js
@@ -1,1 +1,68 @@
 "use strict";
+
+// const btn = document.querySelector("button");
+const overlay = document.querySelector(".overlay");
+const buttons = document.querySelectorAll("button");
+
+/*
+in html page script onclick="alert('Click')"
+
+for small puproses only
+*/
+
+/*
+rare using of this code
+
+hard to track changes
+*/
+// btn.onclick = () => {
+//     alert("Click");
+// };
+
+/*
+the most common and right approach
+
+not missing anything
+*/
+let count = 0;
+
+const deleteMouseEnterCallbackFunction = (event) => {
+    event.target.remove();
+};
+
+const logMouseEnterCallbackFunction = (event) => {
+    console.log(event.type);
+    console.log(event.currentTarget);
+    // if (count == 10) {
+    //     btn.removeEventListener("mouseenter", logMouseEnterCallbackFunction);
+    //     console.log("Finished count");
+    // }
+};
+
+// btn.addEventListener("click", deleteMouseEnterCallbackFunction);
+
+/*
+as a result will be dublicated in console
+*/
+// btn.addEventListener("mouseenter", logMouseEnterCallbackFunction);
+overlay.addEventListener("mouseenter", logMouseEnterCallbackFunction);
+
+// btn.addEventListener("click", () => {
+//     alert("Second Click");
+// });
+
+/*
+cancel the default behavior
+*/
+const link = document.querySelector("a");
+
+// {once: true} means it will work one time only
+link.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    console.log(event.target);
+}, {once: true});
+
+buttons.forEach(btn => {
+    btn.addEventListener("click", logMouseEnterCallbackFunction);
+});
